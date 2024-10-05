d608e69a88d29e78473e2c42a6135491ed19a649
Joe Sanders <kernel528@gmail.com>
2024-10-05 14:49:45 -0500

    Updated src/main.js per lesson.

--- src/main.js
+++ src/main.js
@@ -3,3 +3,13 @@ const { welcome, goodbye, tell } = require("../utils/fortune-teller");
 const promise = welcome();
 
 console.log(promise);
+
+const question = "Will the weather be nice today?";
+const tellPromise = tell(question);
+tellPromise.then((fortune) => {
+    console.log(question);
+    console.log(fortune);
+});
+
+//> Will the weather be nice today?
+//> Without a doubt.
